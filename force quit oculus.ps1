bbc1c59dde6a579c1937af8043a50c2fd3d763a5
Kelsey Higham <kelseyhigham+github@gmail.com>
2025-02-06 17:32:15 -0800

    add script: close all oculus processes

---
+++ force quit oculus.ps1
@@ -0,0 +1,13 @@
+# # Check if the script is running as Administrator
+# $CurrentIdentity = [Security.Principal.WindowsIdentity]::GetCurrent()
+# $Principal = New-Object Security.Principal.WindowsPrincipal($CurrentIdentity)
+# $IsAdmin = $Principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+# if (-not $IsAdmin) {
+#     # If not running as admin, restart the script as admin
+#     # This doesn't work properly, regardless of PS5 (`powershell`) or PS7 (`pwsh`)
+#     Start-Process pwsh -Verb runAs -ArgumentList "-File", $MyInvocation.MyCommand.Path
+#     exit
+# }
+
+Get-Process | Where-Object { $_.Path -match "oculus" -or $_.Name -like "OVR*" } | Stop-Process -Force
